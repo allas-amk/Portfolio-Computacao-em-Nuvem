f994b2b36d24e9098b671175d04c127020c1b1fc
Allas Maycon <51690108+allas-amk@users.noreply.github.com>
2023-09-08 11:58:15 -0300

    Add files via upload

--- Redes/src/redes/Redes.java
+++ Redes/src/redes/Redes.java
@@ -1,10 +1,18 @@
 
 package redes;
 
+import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
+import java.util.Locale;
 import org.cloudbus.cloudsim.Cloudlet;
+import org.cloudbus.cloudsim.Datacenter;
+import org.cloudbus.cloudsim.Host;
+import org.cloudbus.cloudsim.Log;
+import org.cloudbus.cloudsim.Pe;
 import org.cloudbus.cloudsim.Vm;
 import org.cloudbus.cloudsim.core.CloudSim;
+import org.cloudbus.cloudsim.provisioners.PeProvisionerSimple;
 
 /**
  *
@@ -18,14 +26,40 @@ public class Redes {
     // Lista Vm
     private static List<Vm> vmlist;
     
+    @SuppressWarnings("unused")
     public static void main(String[] args) {
         
+        Log.printLine("Iniciando o CloudSim...");
         
+        try {
+            int numero_usuario = 1; // número de usuários dentro da simulação
+            Calendar calendario = Calendar.getInstance(); // usamos para marcar o início da simulação
+            boolean sinalizador_de_rastreamento = false; // é um arquivo que usamos para gerar relatorios
+            
+            // Inicializando o CloudSim
+            CloudSim.init(numero_usuario, calendario, sinalizador_de_rastreamento);
+            
+            // Criando o DataCenter
+            Datacenter datacenter0 = createDatacenter("Datacenter_0");
+            
+        }
+        catch(Exception e) {}
         
-        // inicializando CloudSim
-        Cloudlet.init();
+    }
+
+    private static Datacenter createDatacenter(String datacenter_0) {
         
-        System.out.println("CloudSim inicializado");
+        // Criando o Host dentro de uma lista 
+        List<Host> hostList = new ArrayList<Host>();
+        
+        // Criando o núcleo, que é a classe do CloudSim, que significa os processadores
+        List<Pe> peList = new ArrayList<Pe>();
+        
+        int mips = 1000;
+        
+        peList.add(new Pe(0, new PeProvisionerSimple(mips)));
+        
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
     
 }
